12b30f5396f5b3e39aebbe3e4fbce1d48d966c75
Jayhom <48545313+Jayhom-Jiang@users.noreply.github.com>
2021-11-09 21:38:10 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,17 +1,8 @@
-#include <booster/booster.h>
-#include <booster/sgeconv.h>
-#include <booster/helper.h>
-
-// #include <immintrin.h>
-#include <arm_neon.h>
-#include <string.h>
-#include <assert.h>
-#include <stdio.h>
-
-namespace booster
-{
-int align_ceil(int num, int align)
-{
-    return num + (align - (num % align)) % align;
-}
+class HelloWorld
+{  
+    static void Main () 
+    {     
+        System.Console.Write("Hello World!");  
+    }
+} 
 
